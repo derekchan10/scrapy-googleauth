9ed613d65d3a70d4f0702b44d7a92c93d7f8af4a
陈达超 <chendachao@addcn.com>
2019-09-29 16:54:11 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "scrapy-googleauth",
-    version = "0.0.7",
+    version = "0.1.0",
     keywords = ("pip", "datacanvas", "eds", "xiaoh"),
     description = "google auth downloader middleware for scrapy",
     long_description = "google auth downloader middleware for scrapy",
